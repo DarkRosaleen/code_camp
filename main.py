becddeb0b10af0585c54f0a5fa5f76e788aae2af
Rosaleen <68727159+DarkRosaleen@users.noreply.github.com>
2021-06-21 13:03:36 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+from Leon import arithmetic_arranger
+
+def main():
+    print(arithmetic_arranger(["123 + 456", "789 - 1011", "1213 + 1415", "1617 + 1819"]))
+    print("\n")
+    print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"], True))
+
+
+if __name__ == "__main__":
+    main()
